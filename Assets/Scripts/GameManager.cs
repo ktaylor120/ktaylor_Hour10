5ca2971c010523fcc61a5c4f48e214fedc3e1f89
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-04-13 23:21:41 -0700

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GoalScript blue, green, red, orange;
-    private bool isGameOver = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GoalScript blue, green, red, orange;
+	private bool isGameOver = true;
+    private float elapsedTime = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+    void Update ()
+	{
+        elapsedTime = elapsedTime + Time.deltaTime;
+        // If all four goals are solved then the game is over
+        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;		
+	}
 
-    }
-    private void OnGUI()
-    {
-        if(isGameOver)
-        {
-            Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
-            Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
-            GUI.Label(rect2, "Good Job!");
-        }
-    }
-}
+	void OnGUI()
+	{
+		if (isGameOver)
+		{
+			Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
+			GUI.Box(rect, "Game Over");
+
+			Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
+			GUI.Label(rect2, "Good Job!");
+		}
+		else
+		{
+			GUI.Box(new Rect(Screen.width / 2 - 65, Screen.height - 115, 130, 40), "Your Time Is");
+			GUI.Label(new Rect(Screen.width / 2 - 10, Screen.height - 100, 20, 30), ((int)elapsedTime).ToString());
+		}
+		}
+	}
