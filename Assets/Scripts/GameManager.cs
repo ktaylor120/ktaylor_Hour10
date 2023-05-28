c38fe19797c2c76167c858106d7d0b3697c7186e
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-05-27 22:19:50 -0700

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,20 +13,23 @@ public class GameManager : MonoBehaviour
         isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;		
 	}
 
-	void OnGUI()
-	{
-		if (isGameOver)
-		{
-			Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
-			GUI.Box(rect, "Game Over");
+    void OnGUI()
+    {
+        if (isGameOver)
+        {
+            Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
+            GUI.Box(rect, "Game Over");
 
-			Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
-			GUI.Label(rect2, "Good Job!");
-		}
-		else
-		{
-			GUI.Box(new Rect(Screen.width / 2 - 65, Screen.height - 115, 130, 40), "Your Time Is");
-			GUI.Label(new Rect(Screen.width / 2 - 10, Screen.height - 100, 20, 30), ((int)elapsedTime).ToString());
-		}
-		}
-	}
+            Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
+            GUI.Label(rect2, "Good Job!");
+
+            Rect rect3 = new Rect(Screen.width / 2 - 50, Screen.height / 2 + 25, 100, 30);
+            GUI.Label(rect3, "Time: " + ((int)elapsedTime).ToString());
+        }
+        else
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 65, Screen.height - 115, 130, 40), "Your Time Is");
+            GUI.Label(new Rect(Screen.width / 2 - 10, Screen.height - 100, 20, 30), ((int)elapsedTime).ToString());
+        }
+    }
+}
